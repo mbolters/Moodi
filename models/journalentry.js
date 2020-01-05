8628e36163e6c6f127b6903fc81143a5ed69386e
mbolters <mariematisans@gmail.com>
2020-01-05 12:31:12 -0600

    added models for mongoose

---
+++ models/journalentry.js
@@ -0,0 +1,20 @@
+var mongoose = require ('mongoose');
+
+//article schema
+var Schema = mongoose.Schema;
+var CommentSchema = new Schema({
+    name: {
+        type: String,
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    day: {
+        type: String,
+        required: true
+    },
+})
+
+var Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
