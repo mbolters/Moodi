57dc9366e20ed136f85ca87916782e305fee3911
mbolters <mariematisans@gmail.com>
2020-01-05 21:52:21 -0600

    update routes for user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://moodimarie:mnbmnb@cluster0-jxnwq.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://moodimarie:mnbmnb@cluster0-jxnwq.mongodb.net/test?retryWrites=true&w=majority", 
+    secretOrKey: "secret"
   };
