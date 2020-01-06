16058f797d6025bd74cb393c3145ec93788b7f3b
mbolters <mariematisans@gmail.com>
2020-01-05 21:54:14 -0600

    added users route to server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 //Initialize
 const app = express();
@@ -22,6 +25,15 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
