380e60770e9eea1f4ddbb4f7143f38f453d23e73
Davis Hochstatter <davishochs@gmail.com>
2020-01-09 21:17:50 -0600

    i am the greatest to ever exist

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ const users = require("./routes/api/users");
 const moods = require("./routes/moods");
 
 
+
+
 //Initialize
 const app = express();
+
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // Bodyparser middleware
 app.use(
@@ -20,11 +29,11 @@ app.use(
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+//const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect("mongodb://localhost/moodi", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
@@ -38,5 +47,5 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use('/moods', moods);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port to deploy the app there
+const port = process.env.PORT || 5000 ; // process.env.port is Heroku's port to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
