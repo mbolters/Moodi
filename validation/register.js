193efa38fb250240f1b6fd69d6488039addbe222
Davis Hochstatter <davishochs@gmail.com>
2020-01-12 20:40:59 -0600

    edited DB to take username rather than email, still working on get request for the data

--- validation/register.js
+++ validation/register.js
@@ -7,7 +7,7 @@ module.exports = function validateRegisterInput(data) {
 
 // Convert empty fields to an empty string so we can use validator functions (validator only uses strings)
   data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
@@ -17,11 +17,9 @@ module.exports = function validateRegisterInput(data) {
   }
 
 // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
-  }
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Username field is required";
+  } 
 
 // Password checks
     if (Validator.isEmpty(data.password)) {
