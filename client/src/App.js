e74bd4add763e7daa91cdc8646f6eed7e01fe96b
Davis Hochstatter <davishochs@gmail.com>
2020-01-14 17:59:50 -0600

    GET REQUEST FINALLY WORKS FUCK

--- client/src/App.js
+++ client/src/App.js
@@ -50,8 +50,9 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Navbar />            
+                       
             <MobileFoot />
+            <Route exact path="/davishochs" component={Landing} />
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
