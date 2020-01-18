61f1148753159fe1dd122d9c33555b7fa5491440
Davis Hochstatter <davishochs@gmail.com>
2020-01-18 03:41:43 -0600

    good to go

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -16,7 +16,7 @@ return (
       <ul className="side-nav fixed transparent z-depth-0 hide-on-med-and-down">
 <li className="active"><a href="/dashboard"><i className="material-icons">home</i>Dashboard</a></li>
 <li><a href="/create"><i className="material-icons">add_circle_outline</i>Log Mood</a></li>
-<li><a href="#"><i className="material-icons">face</i>View Moods</a></li>
+<li><a href="/moods"><i className="material-icons">face</i>View Moods</a></li>
 <li>
   <div className="divider"></div>
 </li>
