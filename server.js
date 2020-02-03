bd2be28f0658cc8758b02bb6cbc16d3456dc45e8
mbolters <mariematisans@gmail.com>
2020-02-03 10:48:17 -0600

    heroku routing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const moods = require("./routes/moods");
+const path = require("path");
 
 
 //Initialize
@@ -43,6 +44,15 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use('/moods', moods);
+
+//Serve static assets
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 const port = process.env.PORT || 5000 ; // process.env.port is Heroku's port to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
