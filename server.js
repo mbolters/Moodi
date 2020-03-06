7a19881db763f872786f95ab5fc485c7ca4e3637
Davis Hochstatter <davishochs@gmail.com>
2020-03-06 02:34:59 -0600

    hail mary

--- server.js
+++ server.js
@@ -40,6 +40,17 @@ mongoose
 // Passport middleware
 app.use(passport.initialize());
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Passport config
 require("./config/passport")(passport);
 
